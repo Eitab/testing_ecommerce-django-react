2973c86e3e0829c2bcd450d2bc51f23cd210eb59
“[eitab <[valid-email]>
2022-07-18 11:59:02 +0300

    add test user selenuim

--- tests/E2E/test_selenuim.py
+++ tests/E2E/test_selenuim.py
@@ -1,7 +1,6 @@
 import time
 import pytest
 from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
 
 
 from selenium.webdriver.firefox import webdriver
@@ -19,26 +18,36 @@ from selenium.webdriver.support.wait import WebDriverWait
 def driver():
     firefox_driver_binary = "./geckodriver.exe"
     ser_firefox = firefoxService(firefox_driver_binary)
-    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=ser_firefox)
 
     yield driver
     driver.close()
 
-def test_success_login(driver):
+def test_registration(driver):
     driver.get("http://localhost:8000/")
     driver.maximize_window()
     driver.find_element(By.CSS_SELECTOR,'#navbarScroll > div > a:nth-child(2)').click() #login link
     driver.find_element(By.CSS_SELECTOR,"#root > div > main > div > div > div > div > div > a").click() #register
     driver.find_element(By.CSS_SELECTOR, "#name").send_keys("nancy") #name
-    driver.find_element(By.CSS_SELECTOR, "#email").send_keys("nana987@hotmail.com") #email
+    driver.find_element(By.CSS_SELECTOR, "#email").send_keys("nana79501@hotmail.com") #email
     driver.find_element(By.CSS_SELECTOR, "#password").send_keys("bbbbggggg")
     driver.find_element(By.CSS_SELECTOR, "#passwordConfirm").send_keys("bbbbggggg")
     element = driver.find_element(By.CSS_SELECTOR, "#root > div > main > div > div > div > form > button")
     driver.execute_script("arguments[0].click();", element)
+    time.sleep(5)
+    user_name=driver.find_element(By.CSS_SELECTOR,"#username").text
+    time.sleep(5)
+    assert "NANCY"==user_name
 
-
-
-    # driver.find_element(By.CSS_SELECTOR,"#email").send_keys("toti_loti@hotmail.com")
-    # driver.find_element(By.CSS_SELECTOR, "#password").send_keys("totiloti")
-    # driver.find_element(By.CSS_SELECTOR,"#root > div > main > div > div > div > form > button").click()
-    # time.sleep(3)
+def test_success_login(driver):
+    driver.get("http://localhost:8000/")
+    driver.maximize_window()
+    driver.find_element(By.CSS_SELECTOR,'#navbarScroll > div > a:nth-child(2)').click() #login link
+    driver.find_element(By.CSS_SELECTOR, "#email").send_keys("nana79501@hotmail.com")  # email
+    driver.find_element(By.CSS_SELECTOR, "#password").send_keys("bbbbggggg")
+    element = driver.find_element(By.CSS_SELECTOR, "#root > div > main > div > div > div > form > button")
+    driver.execute_script("arguments[0].click();", element)
+    time.sleep(3)
+    user_name = driver.find_element(By.CSS_SELECTOR, "#username").text
+    time.sleep(5)
+    assert "NANCY" == user_name
